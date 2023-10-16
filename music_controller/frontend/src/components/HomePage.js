a6f605084d6e1c4e5fa639e579d8c43fa465a8e1
flawlessness-professional <mhrabykyan2@gmail.com>
2023-10-16 04:09:35 -0700

    creating the class for home page

---
+++ music_controller/frontend/src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React , { Component } from "react"
+
+// this is the page for None url
+
+// this class is the class that we are rendering in app.j file
+export default class HomePage extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <h1>hello world</h1>;
+    }
+}
