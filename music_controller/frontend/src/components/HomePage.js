c7c78faacb773d6e17f58e05290e4eae162c0fe6
flawlessness-professional <mhrabykyan2@gmail.com>
2023-10-16 04:42:00 -0700

    adding react-router-dom v 5 and making some changes

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,8 +1,12 @@
 import React , { Component } from "react"
 import CreateRoomPage from "./CreateRoomPage"
 import RoomJoinPage from "./RoomJoinPage"
-// this is the page for None url
 
+//imports for react router dom 
+import { BrowserRouter as Router , Switch , } from "react-router-dom"
+//
+
+// this is the page for None url
 // this class is that will handle the page url
 export default class HomePage extends Component {
     constructor(props) {
